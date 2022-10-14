0c0c7702d941ca4e3305cbad1096c8c0dcd96b6e
Emmanuel Aderibigbe <aderibigbeemmanuel2004@gmail.com>
2022-10-14 14:45:50 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: Always 0 (success);
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
