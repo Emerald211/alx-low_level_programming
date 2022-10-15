e06df223501cc6f9c53e315789d0357b67f003cd
Emmanuel Aderibigbe <aderibigbeemmanuel2004@gmail.com>
2022-10-15 02:51:07 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: Always 0;
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
