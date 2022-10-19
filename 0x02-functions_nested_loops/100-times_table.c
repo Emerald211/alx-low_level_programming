372b796e5646ee6e9281df39a9291ac296b6b0a7
Emmanuel Aderibigbe <aderibigbeemmanuel2004@gmail.com>
2022-10-19 13:00:25 -0700

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,37 +12,38 @@ void print_times_table(int n)
 	{
 		for (x = 0; x <= n; x++)
 		{
-			z = x * y;
-
-			if (z > 99)
+			for (y = 0; y <= n; y++)
 			{
-				_putchar(',');
-				_putchar(32);
-				_putchar((z / 100) + '0');
-				_putchar(((z / 10) % 10) + '0');
-				_putchar((z % 10) + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(',');
-				_putchar(32);
-				_putchar(32);
-				_putchar(((z / 10) % 10) + '0');
-				_putchar((z % 10) + '0');
-			}
-			else
-			{
-				if (y != 0)
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
 				{
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
-					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				_putchar(z + '0');
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
 			}
+			_putchar('\n')
 		}
-		_putchar('\n');
 	}
 }
-
