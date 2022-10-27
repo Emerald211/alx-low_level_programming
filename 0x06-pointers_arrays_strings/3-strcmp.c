d3f347289995877b44a864a79925885985b21e20
Emmanuel Aderibigbe <aderibigbeemmanuel2004@gmail.com>
2022-10-27 09:32:48 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- *_strcmp - compares two strings.
- *@s1: pointer to first string.
- *@s2: pointer to second string.
- *
- *Return: value less than 0 if string is less than the other.
- *value greater than 0 if string is greater than the other.
- *0 if strings are equal.
+ * _strcmp - compares two strings.
+ * @s1: pointer to first string.
+ * @s2: pointer to second string.
+ * Return: value less than 0 if string is less than the other.
+ * value greater than 0 if string is greater than the other.
+ * 0 if strings are equal.
  */
 int _strcmp(char *s1, char *s2)
 {
